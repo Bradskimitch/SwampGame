8c2057266a4ac0b397b1d2e90c3b6cdcafde5f19
Ross Manclark <r.manclark@outlook.com>
2018-12-14 12:50:37 +0000

    Swamp game

--- AdventureGame/src/main/java/AdventureGame/App.java
+++ AdventureGame/src/main/java/AdventureGame/App.java
@@ -5,7 +5,7 @@ public class App {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		AdventureGame game = new AdventureGame();
-		FeatureOfInterest goal = new FeatureOfInterest("Huge Sack","It's a treasure trove of christmas presents.",51,51);
+		FeatureOfInterest goal = new FeatureOfInterest("Huge Sack","It's a treasure trove of christmas presents.");
 		FeatureOfInterest sleigh = new FeatureOfInterest("Crashed Sleigh","It's a crashed sleigh.");
 		FeatureOfInterest tree = new FeatureOfInterest("Decorated Tree","It's a tree covered in lights and baubles.");
 		FeatureOfInterest rHat = new FeatureOfInterest("Red Hat","It's a strange red hat with a fluffy bobble on the end.");
